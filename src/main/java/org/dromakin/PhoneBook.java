0568416a97b4c9e329d62df77cbebc163914b63b
Dmitriy Romakin <32188375+dromakin@users.noreply.github.com>
2023-05-03 10:48:20 +0300

    add implemented

--- src/main/java/org/dromakin/PhoneBook.java
+++ src/main/java/org/dromakin/PhoneBook.java
@@ -14,7 +14,24 @@ package org.dromakin;
 
 public class PhoneBook {
 
+    private int countContacts;
+
+    public PhoneBook() {
+        countContacts = 0;
+    }
+
     public int add(String name, String number) {
-        return 0;
+        if (name.matches("[0-9]+")) {
+            throw new IllegalArgumentException("Name is not number!");
+        }
+
+        if (number.matches("[a-zA-Z]+")) {
+            throw new IllegalArgumentException("Number is not name!");
+        }
+
+        // save in db
+        // code here
+
+        return ++countContacts;
     }
 }
