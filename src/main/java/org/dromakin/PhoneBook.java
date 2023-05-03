2afd39c405ecf144c3fc5250cabfb29ba2ace1b1
Dmitriy Romakin <32188375+dromakin@users.noreply.github.com>
2023-05-03 10:29:40 +0300

    Initial commit

---
+++ src/main/java/org/dromakin/PhoneBook.java
@@ -0,0 +1,16 @@
+/*
+ * File:     PhoneBook
+ * Package:  org.dromakin
+ * Project:  netology_tdd
+ *
+ * Created by dromakin as 03.05.2023
+ *
+ * author - dromakin
+ * maintainer - dromakin
+ * version - 2023.05.03
+ * copyright - ORGANIZATION_NAME Inc. 2023
+ */
+package org.dromakin;
+
+public class PhoneBook {
+}
---
+++ src/test/java/org/dromakin/PhoneBookTest.java
@@ -0,0 +1,15 @@
+package org.dromakin;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+class PhoneBookTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
